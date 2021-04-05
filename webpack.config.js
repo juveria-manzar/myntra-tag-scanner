4283e9156773b6244b28f4ac52ee633bd912c8a1
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-05 17:05:17 +0530

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
 
 module.exports = {
   entry: './app/js/main.js',
@@ -18,32 +11,6 @@ module.exports = {
     contentBase: __dirname + '/app'
   },
   optimization: {},
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    new WorkboxPlugin.GenerateSW(),
-    new HtmlWebpackPlugin({
-      template: './app/index.html',
-      minify: {
-        collapseWhitespace: true
-      }
-    }),
-    new ExtractTextPlugin({
-      filename: 'styles.css'
-    }),
-    new OptimizeCssAssetsPlugin({
-      cssProcessorPluginOptions: {
-        preset: ['default', { discardComments: { removeAll: true } }]
-      }
-    }),
-    new CopyWebpackPlugin([{ from: 'images/', to: 'images' }, 'decoder.js', 'manifest.json', 'CNAME'], {
-      context: './app'
-    }),
-    new SitemapPlugin('https://qrcodescan.in', ['/'])
-  ],
   module: {
     rules: [
       {
